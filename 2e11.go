59b69281e1ce2e9f4d58386d7ace38945c826b89
Leandro Guillen <leandroguillen@gmail.com>
2014-07-24 22:22:30 +0200

    More cleanup

--- 2e11.go
+++ 2e11.go
@@ -2,16 +2,12 @@ package main
 
 import "fmt"
 import "time"
-// import "math/rand"
+
 
 import "github.com/LeandroGuillen/2e11/aiplayer"
 import "github.com/LeandroGuillen/2e11/strategy"
 
 func main() {
-//   r := rand.New(rand.NewSource(time.Now().UnixNano()))
-
-  
-
   score := 0
   highest := 0
   lowest := 2000000
@@ -24,47 +20,17 @@ func main() {
   fmt.Println("Strategy:", s.Name(), "...")
 
   for i := 0; i < games; i++ {
-//     fmt.Println("Playing game", i, "...")
-    
     go func(c chan int) {
       p := aiplayer.Player{Name: "Paco"}
       p.Init()
-//       c <- r.Intn(15000)
       p.Play(s, c)
     }(c)
-
-    
-    //score = p.Score
-//     score = <- c
-
-//         select {
-//     case score = <- c:
-//         
-//       fmt.Println("score:", score)
-//       if score > highest {
-//         highest = score
-//       }
-//       
-//       if score < lowest {
-//         lowest = score
-//       }
-//       
-//       average = average + score
-//       
-//     case <-time.After(time.Second * 1):
-//       fmt.Println("I'm done")
-//       
-//       break
-//     }
   }
  
-  
-  
   for x := 0; x < games; x++{
     select {
     case score = <- c:
         
-//       fmt.Println("Consuming score:", score)
       if score > highest {
         highest = score
       }
